77ee9adaeda90c9d888b7eaf4a1b51de1219f6d6
Raffael Kessler <raffael.kessler@prowect.com>
2016-05-28 11:34:02 +0200

    Logfile hinzugefügt

--- drips.php
+++ drips.php
@@ -3,10 +3,12 @@
 use Drips\App;
 use Drips\Logger\Logger;
 use Drips\Logger\Handler;
+use Monolog\Handler\StreamHandler;
 
 if (class_exists('Drips\App')) {
     App::on('create', function (App $app) {
         $app->logger = new Logger('dripslogger');
+        $app->logger->pushHandler(new StreamHandler(DRIPS_LOGS.'/drips.log'));
         $app->logger->pushHandler(new Handler);
     });
 }
